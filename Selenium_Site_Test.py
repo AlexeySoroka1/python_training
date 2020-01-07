adac56b8944753ad65daaedff99204ce63b15201
AlexeySoroka1 <47444861+AlexeySoroka1@users.noreply.github.com>
2020-01-07 12:59:09 +0200

    Edited file Slenium_Site_Test, changge browser from FireFox t Chrome and set window size

--- Selenium_Site_Test.py
+++ Selenium_Site_Test.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest, re
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
+        self.driver.set_window_size(1920, 1080)
         self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
